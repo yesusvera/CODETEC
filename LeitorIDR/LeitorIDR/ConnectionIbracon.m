68deb99233d7770d518ba6e7135331d20359f568
Jonathan Jordan Carrillo Salcedo <JJCS@MacBook-Pro-de-Jonathan.local>
2014-02-24 21:58:50 -0300

    Conclusao do Parse do XML estante

--- LeitorIDR/LeitorIDR/ConnectionIbracon.m
+++ LeitorIDR/LeitorIDR/ConnectionIbracon.m
@@ -67,111 +67,112 @@
 
 -(void)registrarDispositivo:(NSString *)_url indicadorCarregando:(UIActivityIndicatorView *)indicadorAtividade controller:(UIViewController *)controlador documento:(NSString *) documento senha:(NSString *) senha{
    
-    NSString *estante = [[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:@"Registrar.xml"];
+    //BUSCA LOCAL DO XML
+//    NSString *estante = [[[NSBundle mainBundle]resourcePath] stringByAppendingPathComponent:@"Registrar.xml"];
+//    
+//    NSData *data = [[NSData alloc] initWithContentsOfFile:estante];
+//    NSString *corpoXML = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//    NSLog(@"%@", corpoXML);
+//    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
+//    NSLog(@"%@", parser);
+//    [parser setDelegate:self];
+//    
+//    if(![parser parse]){
+//        NSLog(@"Erro ao realizar o parse");
+//    }else{
+//        NSLog(@"Ok Parse");
+//    }
+//
+//    
+//    EstantesController *estanteController = [[EstantesController alloc] init];
+//    [estanteController setRegistrarLivroResponse:registrarLivroResponse];
+//    [estanteController setTxtDocumento : documento];
+//    [estanteController setTxtSenha : senha];
+//    [controlador.navigationController pushViewController:estanteController animated:YES];
     
-    NSData *data = [[NSData alloc] initWithContentsOfFile:estante];
-    NSString *corpoXML = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"%@", corpoXML);
-    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
-    NSLog(@"%@", parser);
-    [parser setDelegate:self];
     
-    if(![parser parse]){
-        NSLog(@"Erro ao realizar o parse");
-    }else{
-        NSLog(@"Ok Parse");
-    }
-
     
-    EstantesController *estanteController = [[EstantesController alloc] init];
-    [estanteController setRegistrarLivroResponse:registrarLivroResponse];
-    [estanteController setTxtDocumento : documento];
-    [estanteController setTxtSenha : senha];
-    [controlador.navigationController pushViewController:estanteController animated:YES];
+    NSOperationQueue *networkQueue = [[NSOperationQueue alloc] init];
+    networkQueue.maxConcurrentOperationCount = 5;
     
-//    DESCOMENTAR QUANDO USAR O WEBSERVICE
-//    
-//    NSOperationQueue *networkQueue = [[NSOperationQueue alloc] init];
-//    networkQueue.maxConcurrentOperationCount = 5;
-//    
-//    NSLog(@"%@", _url);
-//    
-//    NSURL *url = [NSURL URLWithString:_url];
-//    
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-//    NSLog(@"%@", operation);
-//    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSString *respostaXML = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
-//        NSLog(@"%@", respostaXML);
-//        
-//        [indicadorAtividade stopAnimating];
-//        indicadorAtividade.hidden = YES;
-//        
-//        //FAZENDO O PARSE XML
-//        NSData *respDataXML = [respostaXML dataUsingEncoding:NSUTF8StringEncoding];
-//        NSLog(@"%@", respostaXML);
-//        NSXMLParser *parser = [[NSXMLParser alloc] initWithData:respDataXML];
-//        [parser setDelegate:self];
-//        
-//        if(![parser parse]){
-//            NSLog(@"Erro ao realizar o parse");
-//        }else{
-//            NSLog(@"Ok Parse");
-//        }
-//        
-//        
-//        NSString *mensagemAlerta = registrarLivroResponse.status;
-//        if(![registrarLivroResponse.erro isEqualToString:@"0"]){
-//            mensagemAlerta = [mensagemAlerta stringByAppendingString:@" - "];
-//            mensagemAlerta = [mensagemAlerta stringByAppendingString:registrarLivroResponse.msgErro];
-//        }
-//        
-//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Registro "
-//                                                        message:mensagemAlerta
-//                                                       delegate:nil
-//                                              cancelButtonTitle:@"OK"
-//                                              otherButtonTitles:nil
-//                              
-//                              ];
-//        
-//        [alert show];
-//        
-//        
-//        //REDIRECIONANDO PARA AS ESTANTES
-//        if([registrarLivroResponse.erro isEqualToString:@"0"] & [[registrarLivroResponse.status lowercaseString] isEqualToString:@"ativado"]){
-//            EstantesController *estanteController = [[EstantesController alloc] init];
-//            [estanteController setRegistrarLivroResponse:registrarLivroResponse];
-//            [estanteController setTxtDocumento : documento];
-//            [estanteController setTxtSenha : senha];
-//            [controlador.navigationController pushViewController:estanteController animated:YES];
-//            
-//        }
-//        
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        NSLog(@"%s: AFHTTPRequestOperation error: %@", __FUNCTION__, error);
-//        
-//        UIAlertView *alertError = [
-//                                   [UIAlertView alloc] initWithTitle:@"Erro"
-//                                   message:error.description
-//                                   delegate:nil
-//                                   cancelButtonTitle:@"Visto"
-//                                   otherButtonTitles:nil
-//                                   ];
-//        
-//        
-//        [alertError show];
-//        
-//        [indicadorAtividade stopAnimating];
-//        indicadorAtividade.hidden = YES;
-//        
-//    }];
-//    
-//    indicadorAtividade.hidden = NO;
-//    [indicadorAtividade startAnimating];
-//    
-//    
-//    [networkQueue addOperation:operation];
+    NSLog(@"%@", _url);
+    
+    NSURL *url = [NSURL URLWithString:_url];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    NSLog(@"%@", operation);
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *respostaXML = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", respostaXML);
+        
+        [indicadorAtividade stopAnimating];
+        indicadorAtividade.hidden = YES;
+        
+        //FAZENDO O PARSE XML
+        NSData *respDataXML = [respostaXML dataUsingEncoding:NSUTF8StringEncoding];
+        NSLog(@"%@", respostaXML);
+        NSXMLParser *parser = [[NSXMLParser alloc] initWithData:respDataXML];
+        [parser setDelegate:self];
+        
+        if(![parser parse]){
+            NSLog(@"Erro ao realizar o parse");
+        }else{
+            NSLog(@"Ok Parse");
+        }
+        
+        
+        NSString *mensagemAlerta = registrarLivroResponse.status;
+        if(![registrarLivroResponse.erro isEqualToString:@"0"]){
+            mensagemAlerta = [mensagemAlerta stringByAppendingString:@" - "];
+            mensagemAlerta = [mensagemAlerta stringByAppendingString:registrarLivroResponse.msgErro];
+        }
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Registro "
+                                                        message:mensagemAlerta
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil
+                              
+                              ];
+        
+        [alert show];
+        
+        
+        //REDIRECIONANDO PARA AS ESTANTES
+        if([registrarLivroResponse.erro isEqualToString:@"0"] & [[registrarLivroResponse.status lowercaseString] isEqualToString:@"ativado"]){
+            EstantesController *estanteController = [[EstantesController alloc] init];
+            [estanteController setRegistrarLivroResponse:registrarLivroResponse];
+            [estanteController setTxtDocumento : documento];
+            [estanteController setTxtSenha : senha];
+            [controlador.navigationController pushViewController:estanteController animated:YES];
+            
+        }
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%s: AFHTTPRequestOperation error: %@", __FUNCTION__, error);
+        
+        UIAlertView *alertError = [
+                                   [UIAlertView alloc] initWithTitle:@"Erro"
+                                   message:error.description
+                                   delegate:nil
+                                   cancelButtonTitle:@"Visto"
+                                   otherButtonTitles:nil
+                                   ];
+        
+        
+        [alertError show];
+        
+        [indicadorAtividade stopAnimating];
+        indicadorAtividade.hidden = YES;
+        
+    }];
+    
+    indicadorAtividade.hidden = NO;
+    [indicadorAtividade startAnimating];
+    
+    
+    [networkQueue addOperation:operation];
 }
 
 
